3ccf234673b313cac813aa2a9d51ac9625225347
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-15 18:05:03 +0530

    Add getAppointments route in appointmentRouter

--- Server/routes/appointmentRouter.js
+++ Server/routes/appointmentRouter.js
@@ -1,1 +1,9 @@
 // MediFlow / Server / routes / appointmentRouter.js
+import express from "express";
+import { getAppointments } from "../controllers/appointmentController.js";
+
+const appointmentRouter = express.Router();
+
+appointmentRouter.get("/appointments-get", getAppointments);
+
+export default appointmentRouter;
