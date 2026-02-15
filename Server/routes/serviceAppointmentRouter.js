d7379efe61bb1478167786108d0ae88b806b0a45
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-15 23:11:28 +0530

    Add getServiceAppointments route in serviceAppointmentRouter

--- Server/routes/serviceAppointmentRouter.js
+++ Server/routes/serviceAppointmentRouter.js
@@ -3,10 +3,16 @@ import express from "express";
 import {
   confirmServiceAppointmentPayment,
   createServiceAppointment,
+  getServiceAppointments,
 } from "../controllers/serviceAppointmentController.js";
 import { clerkMiddleware, requireAuth } from "@clerk/express";
 
 const serviceAppointmentRouter = express.Router();
+
+serviceAppointmentRouter.get(
+  "/serviceAppointments-get",
+  getServiceAppointments,
+);
 
 serviceAppointmentRouter.get(
   "/serviceAppointment-confirm-payment",
