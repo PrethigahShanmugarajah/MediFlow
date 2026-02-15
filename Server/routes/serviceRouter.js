ee68703724fa96a4e63d353e12650e3d1aef7b6a
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-15 14:35:28 +0530

    Add createService route in serviceRouter

--- Server/routes/serviceRouter.js
+++ Server/routes/serviceRouter.js
@@ -1,1 +1,12 @@
 // MediFlow / Server / routes / serviceRouter.js
+import express from "express";
+import multer from "multer";
+import { createService } from "../controllers/serviceController.js";
+
+const upload = multer({ dest: "/tmp" });
+
+const serviceRouter = express.Router();
+
+serviceRouter.post("/service-create", upload.single("image"), createService);
+
+export default serviceRouter;
