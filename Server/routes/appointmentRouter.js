f5e7aac83f808870532081d88be81654327cf7da
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-15 19:04:59 +0530

    Add getAppointmentsByPatient route in appointmentRouter

--- Server/routes/appointmentRouter.js
+++ Server/routes/appointmentRouter.js
@@ -1,9 +1,20 @@
 // MediFlow / Server / routes / appointmentRouter.js
 import express from "express";
-import { getAppointments } from "../controllers/appointmentController.js";
+import {
+  getAppointments,
+  getAppointmentsByPatient,
+} from "../controllers/appointmentController.js";
+import { clerkMiddleware, requireAuth } from "@clerk/express";
 
 const appointmentRouter = express.Router();
 
 appointmentRouter.get("/appointments-get", getAppointments);
+
+appointmentRouter.get(
+  "/appointment-get-by-patient",
+  clerkMiddleware(),
+  requireAuth(),
+  getAppointmentsByPatient,
+);
 
 export default appointmentRouter;
