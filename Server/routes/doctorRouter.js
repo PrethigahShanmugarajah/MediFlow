c9a96925e5ec88f152914725c6d9a6ed627f42df
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-15 12:28:16 +0530

    Add getDoctor route in doctorRouter

--- Server/routes/doctorRouter.js
+++ Server/routes/doctorRouter.js
@@ -1,13 +1,18 @@
 // MediFlow / Server / routes / doctorRouter.js
 import express from "express";
 import multer from "multer";
-import { createDoctor, getDoctors } from "../controllers/doctorController.js";
+import {
+  createDoctor,
+  getDoctor,
+  getDoctors,
+} from "../controllers/doctorController.js";
 
 const upload = multer({ dest: "/tmp" });
 
 const doctorRouter = express.Router();
 
 doctorRouter.get("/doctors-get", getDoctors);
+doctorRouter.get("/doctor-get/:id", getDoctor);
 doctorRouter.post("/doctor-create", upload.single("image"), createDoctor);
 
 export default doctorRouter;
