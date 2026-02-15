bdd38ef8e0a3d5a605c6f5a7d42a7da055ba0720
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-15 22:27:34 +0530

    Add serviceAppointmentRouter

--- Server/server.js
+++ Server/server.js
@@ -7,9 +7,28 @@ import connectDB from "./config/db.js";
 import doctorRouter from "./routes/doctorRouter.js";
 import serviceRouter from "./routes/serviceRouter.js";
 import appointmentRouter from "./routes/appointmentRouter.js";
+import serviceAppointmentRouter from "./routes/serviceAppointmentRouter.js";
 
 /* -------- INITIALIZE EXPRESS -------- */
 const app = express();
+
+const allowedOrigins = [process.env.FRONTEND_URL, process.env.ADMIN_URL];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      return callback(new Error("Not allowed by CORS."));
+    },
+    credentials: true,
+    methods: ["GET", "POST", "POST", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+  }),
+);
 
 /* -------- CONNECT TO DATABASE -------- */
 connectDB();
@@ -25,6 +44,7 @@ app.get("/", (req, res) => res.send("API is Working!"));
 app.use("/api/doctors", doctorRouter);
 app.use("/api/services", serviceRouter);
 app.use("/api/appointments", appointmentRouter);
+app.use("/api/serviceAppointments", serviceAppointmentRouter);
 
 /* -------- PORT -------- */
 const port = process.env.PORT || 5000;
