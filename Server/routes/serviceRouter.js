f9fa0ada1ab3baa1bcb511cd2931fc7a233fc702
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-15 14:42:46 +0530

    Add getServices route in serviceRouter

--- Server/routes/serviceRouter.js
+++ Server/routes/serviceRouter.js
@@ -1,12 +1,16 @@
 // MediFlow / Server / routes / serviceRouter.js
 import express from "express";
 import multer from "multer";
-import { createService } from "../controllers/serviceController.js";
+import {
+  createService,
+  getServices,
+} from "../controllers/serviceController.js";
 
 const upload = multer({ dest: "/tmp" });
 
 const serviceRouter = express.Router();
 
+serviceRouter.get("/services-get", getServices);
 serviceRouter.post("/service-create", upload.single("image"), createService);
 
 export default serviceRouter;
