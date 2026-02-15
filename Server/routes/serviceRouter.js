b95ba54d4f7252003b92a551b09d8df7aa40968c
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-15 15:04:52 +0530

    Add updateService route in serviceRouter

--- Server/routes/serviceRouter.js
+++ Server/routes/serviceRouter.js
@@ -5,6 +5,7 @@ import {
   createService,
   getServiceById,
   getServices,
+  updateService,
 } from "../controllers/serviceController.js";
 
 const upload = multer({ dest: "/tmp" });
@@ -14,5 +15,6 @@ const serviceRouter = express.Router();
 serviceRouter.get("/services-get", getServices);
 serviceRouter.get("/service-get/:id", getServiceById);
 serviceRouter.post("/service-create", upload.single("image"), createService);
+serviceRouter.put("/service-update/:id", upload.single("image"), updateService);
 
 export default serviceRouter;
