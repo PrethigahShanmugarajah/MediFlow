90f01d5b70f4a3b3348c8c9a64798beb20d24ae3
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-17 12:37:22 +0530

    Update Home page to include Navbar and DashboardPage components

--- Admin/src/pages/Home/Home.jsx
+++ Admin/src/pages/Home/Home.jsx
@@ -1,8 +1,15 @@
 // MediFlow / Admin / src / pages / Home / Home.jsx
 import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
+import DashboardPage from "../../components/DashboardPage/DashboardPage";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-linear-to-br from-blue-50 via-blue-100 to-white">
+      <Navbar />
+      <DashboardPage />
+    </div>
+  );
 };
 
 export default Home;
