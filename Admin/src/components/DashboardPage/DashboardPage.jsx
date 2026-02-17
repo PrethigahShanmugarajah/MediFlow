34c4e92cb346b4ae2cdf110be76269274b07a87a
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-17 22:27:47 +0530

    Update DashboardPage overview text

--- Admin/src/components/DashboardPage/DashboardPage.jsx
+++ Admin/src/components/DashboardPage/DashboardPage.jsx
@@ -125,7 +125,7 @@ const DashboardPage = () => {
               Dashboard
             </h1>
             <p className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-4 mb-6">
-              Overview of doctors & appointments
+              Doctors and appointments at a glance
             </p>
           </div>
         </div>
