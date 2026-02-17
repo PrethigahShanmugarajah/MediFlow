ff1bc7e499f6a33fbb66daf32db903591bac5195
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-17 12:13:46 +0530

    Add DOCTORS_GET, DOCTORS_CREATE and APPOINTMENT_GET_REGISTERED_USERCOUNT API routes

---
+++ Admin/src/api/api_route.js
@@ -0,0 +1,14 @@
+// MediFlow / Admin / src / api / api_route.js
+const BASE_URL = import.meta.env.VITE_BASEURL;
+
+const API_ROUTES = {
+  DOCTORS: {
+    DOCTORS_GET: `${BASE_URL}/api/doctors/doctors-get?limit=200`,
+    DOCTORS_CREATE: `${BASE_URL}/api/doctors/doctor-create`,
+  },
+  APPOINTMENT: {
+    APPOINTMENT_GET_REGISTERED_USERCOUNT: `${BASE_URL}/api/appointments/appointment-get-registered-usercount`,
+  },
+};
+
+export default API_ROUTES;
