8b5dbada6e1bf219ea43d3e03c560400279ee354
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-18 23:19:25 +0530

    Add Navbar and background gradient to Add Service page

--- Admin/src/pages/AddService/AddService.jsx
+++ Admin/src/pages/AddService/AddService.jsx
@@ -1,10 +1,11 @@
 // MediFlow / Admin / src / pages / AddService / AddService.jsx
-import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
 import AddServicePage from "../../components/AddServicePage/AddServicePage";
 
 const AddService = () => {
   return (
-    <div>
+    <div className="bg-linear-to-br from-indigo-50 via-indigo-100 to-fuchsia-50">
+      <Navbar />
       <AddServicePage />
     </div>
   );
