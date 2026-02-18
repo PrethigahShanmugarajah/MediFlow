88390c3fcdf737adfd799f4486415f3fecc4fa14
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-18 12:31:13 +0530

    Add Navbar and background gradient to service dashboard page

--- Admin/src/pages/ServiceDashboard/ServiceDashboard.jsx
+++ Admin/src/pages/ServiceDashboard/ServiceDashboard.jsx
@@ -1,10 +1,11 @@
 // MediFlow / Admin / src / pages / ServiceDashboard / ServiceDashboard.jsx
-import React from "react";
+import Navbar from "../../components/Navbar/Navbar";
 import ServiceDashboardPage from "../../components/ServiceDashboardPage/ServiceDashboardPage";
 
 const ServiceDashboard = () => {
   return (
-    <div>
+    <div className="bg-linear-to-b from-indigo-50 via-indigo-25 to-white">
+      <Navbar />
       <ServiceDashboardPage />
     </div>
   );
