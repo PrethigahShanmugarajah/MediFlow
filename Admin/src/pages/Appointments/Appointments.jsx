ae5b1af168691902e3019f2a2b5accc486a42b3b
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-20 01:31:26 +0530

    Remove the navbar and background wrapper from the Appointments page

--- Admin/src/pages/Appointments/Appointments.jsx
+++ Admin/src/pages/Appointments/Appointments.jsx
@@ -1,11 +1,9 @@
 // MediFlow / Admin / src / pages / Appointments / Appointments.jsx
 import AppointmentsPage from "../../components/AppointmentsPage/AppointmentsPage";
-import Navbar from "../../components/Navbar/Navbar";
 
 const Appointments = () => {
   return (
-    <div className="bg-indigo-50">
-      <Navbar />
+    <div>
       <AppointmentsPage />
     </div>
   );
