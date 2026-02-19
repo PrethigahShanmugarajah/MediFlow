4f83f2fa982d9f51ed6274b833d24e3c90d6997b
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-20 01:30:58 +0530

    Remove the navbar and background wrapper from the AddService page

--- Admin/src/pages/AddService/AddService.jsx
+++ Admin/src/pages/AddService/AddService.jsx
@@ -1,11 +1,9 @@
 // MediFlow / Admin / src / pages / AddService / AddService.jsx
-import Navbar from "../../components/Navbar/Navbar";
 import AddServicePage from "../../components/AddServicePage/AddServicePage";
 
 const AddService = () => {
   return (
-    <div className="bg-linear-to-br from-indigo-50 via-indigo-100 to-fuchsia-50">
-      <Navbar />
+    <div>
       <AddServicePage />
     </div>
   );
