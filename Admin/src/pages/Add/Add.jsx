c6868a6df6fd366cc3c2514d72b623c2656e640f
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-20 01:11:37 +0530

    Remove Navbar and background gradient from Add page

--- Admin/src/pages/Add/Add.jsx
+++ Admin/src/pages/Add/Add.jsx
@@ -1,11 +1,9 @@
 // MediFlow / Admin / src / pages / Add / Add.jsx
 import AddPage from "../../components/AddPage/AddPage";
-import Navbar from "../../components/Navbar/Navbar";
 
 const Add = () => {
   return (
-    <div className="bg-linear-to-br from-indigo-50 via-white to-blue-50">
-      <Navbar />
+    <div>
       <AddPage />
     </div>
   );
