d7e0a101b8d1bf30602b298ae1655ec0e9a085f1
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-20 01:32:45 +0530

    Remove the navbar and background wrapper from the List page

--- Admin/src/pages/List/List.jsx
+++ Admin/src/pages/List/List.jsx
@@ -1,11 +1,9 @@
 // MediFlow / Admin / src / pages / List / List.jsx
 import ListPage from "../../components/ListPage/ListPage";
-import Navbar from "../../components/Navbar/Navbar";
 
 const List = () => {
   return (
-    <div className="bg-indigo-50">
-      <Navbar />
+    <div>
       <ListPage />
     </div>
   );
