d129a6ad612853e050d1034f5476d706d88360c7
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-20 01:17:43 +0530

    Remove Navbar and background styling from the ServiceDashboard page

--- Admin/src/pages/ServiceDashboard/ServiceDashboard.jsx
+++ Admin/src/pages/ServiceDashboard/ServiceDashboard.jsx
@@ -1,11 +1,9 @@
 // MediFlow / Admin / src / pages / ServiceDashboard / ServiceDashboard.jsx
-import Navbar from "../../components/Navbar/Navbar";
 import ServiceDashboardPage from "../../components/ServiceDashboardPage/ServiceDashboardPage";
 
 const ServiceDashboard = () => {
   return (
-    <div className="bg-linear-to-b from-indigo-50 via-indigo-25 to-white">
-      <Navbar />
+    <div>
       <ServiceDashboardPage />
     </div>
   );
