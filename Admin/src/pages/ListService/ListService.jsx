b3b790c18d6be83e1aa18ceb0a86df149f7fd168
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-20 01:33:10 +0530

    Remove unused React import from the ListService page

--- Admin/src/pages/ListService/ListService.jsx
+++ Admin/src/pages/ListService/ListService.jsx
@@ -1,5 +1,4 @@
 // MediFlow / Admin / src / pages / ListService / ListService.jsx
-import React from "react";
 import ListServicePage from "../../components/ListServicePage/ListServicePage";
 
 const ListService = () => {
