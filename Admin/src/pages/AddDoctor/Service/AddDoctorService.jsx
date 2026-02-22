0561746194fa0d12a2867633358375e5342d73bd
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-23 02:27:02 +0530

    Add addDoctor service function to create doctor and handle preview data

---
+++ Admin/src/pages/AddDoctor/Service/AddDoctorService.jsx
@@ -0,0 +1,17 @@
+// MediFlow / Admin / src / pages / AddDoctor / Service / AddDoctorService.jsx
+import { createDoctor } from "../../../services/mutations";
+import { makeDoctorPreviewFromResponse } from "../../../utils/addDoctorUtils";
+
+export async function addDoctor(formData, formForFallback) {
+  const data = await createDoctor(formData);
+
+  if (data?.token) {
+    try {
+      localStorage.setItem("token", data.token);
+    } catch {}
+  }
+
+  const doctor = makeDoctorPreviewFromResponse(data, formForFallback);
+
+  return { data, doctor };
+}
