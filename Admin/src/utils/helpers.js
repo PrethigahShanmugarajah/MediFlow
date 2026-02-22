353eacc35ba1bbebe36403f8e277f5c9d1f629f8
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-22 20:46:53 +0530

    Add CURRENCY constant from environment variables

--- Admin/src/utils/helpers.js
+++ Admin/src/utils/helpers.js
@@ -1,3 +1,4 @@
 // MediFlow / Admin / src / utils / helpers.js
 
+/* -------- Get the currency symbol or code from environment variables -------- */
 export const CURRENCY = import.meta.env.VITE_CURRENCY;
