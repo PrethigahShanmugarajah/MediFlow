26f84a2328c9bcf1e4b531e23aaf7a40405c9349
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-24 01:56:03 +0530

    Add ServiceTable component to display services on mobile, tablet, and desktop with loading and error states

---
+++ Admin/src/pages/ServiceDashboard/Components/ServiceTable.jsx
@@ -0,0 +1,64 @@
+import { ClipLoader } from "react-spinners";
+import ServiceRowTablet from "./ServiceRowTablet";
+import ServiceRowDesktop from "./ServiceRowDesktop";
+import ServiceRowMobile from "./ServiceRowMobile";
+
+const ServiceTable = ({ loading, error, services }) => {
+  return (
+    <div className="bg-white rounded-2xl shadow-sm overflow-hidden border-b border-transparent">
+      {/* -------- Tablet Screen -------- */}
+      <div className="hidden md:grid lg:hidden grid-cols-5 items-center gap-6 px-4 py-3 text-sm text-gray-600 bg-indigo-50">
+        <div className="text-center text-xs font-medium">Service</div>
+        <div className="text-center text-xs font-medium">Appointments</div>
+        <div className="text-center text-xs font-medium">Completed</div>
+        <div className="text-center text-xs font-medium">Canceled</div>
+        <div className="text-center text-xs font-medium">Earning</div>
+      </div>
+
+      {/* -------- Desktop Screen -------- */}
+      <div className="hidden lg:grid md:text-xs lg:text-xs xl:text-md grid-cols-12 items-center gap-4 px-4 py-3 text-sm text-gray-600 bg-indigo-50">
+        <div className="col-span-5">Service</div>
+        <div className="col-span-2">Price</div>
+        <div className="col-span-1 text-center text-xs font-medium">
+          Appointments
+        </div>
+        <div className="col-span-1 text-center text-xs font-medium">
+          Completed
+        </div>
+        <div className="col-span-1 text-center text-xs font-medium">
+          Canceled
+        </div>
+        <div className="col-span-2 text-center">Earning</div>
+      </div>
+
+      <div className="divide-y divide-transparent min-w-full">
+        {loading ? (
+          <div className="px-4 py-6 text-center text-gray-500">
+            <ClipLoader size={50} color="#6366F1" />
+          </div>
+        ) : error ? (
+          <div className="px-4 py-6 text-center text-red-600">{error}</div>
+        ) : services.length === 0 ? (
+          <div className="px-4 py-6 text-center text-gray-500">
+            No services found.
+          </div>
+        ) : (
+          services.map((s) => {
+            return (
+              <div
+                key={s.id}
+                className="px-4 py-4 font-serif hover:shadow-md transition bg-white md:bg-transparent"
+              >
+                <ServiceRowTablet s={s} />
+                <ServiceRowDesktop s={s} />
+                <ServiceRowMobile s={s} />
+              </div>
+            );
+          })
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default ServiceTable;
