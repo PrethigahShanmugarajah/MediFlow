a66b6425261b80b234237c32db10caf8e058a204
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-24 01:46:16 +0530

    Add ServiceRowDesktop component to display service details on desktop

---
+++ Admin/src/pages/ServiceDashboard/Components/ServiceRowDesktop.jsx
@@ -0,0 +1,36 @@
+// MediFlow / Admin / src / pages / ServiceDashboard / Components / ServiceRowDesktop.jsx
+import { formatLKR } from "../../../utils/serviceDashboardUtils";
+
+const ServiceRowDesktop = ({ s }) => {
+  const earning = (s?.completed || 0) * (s?.price || 0);
+
+  return (
+    <div className="hidden lg:grid grid-cols-12 items-center gap-4 cursor-pointer">
+      <div className="col-span-5 flex items-center gap-4">
+        <div className="w-16 h-16 rounded-xl overflow-hidden ring-1 ring-indigo-100 bg-gray-200">
+          <img
+            src={s.image}
+            alt={s.name}
+            className="w-full h-full object-cover"
+          />
+        </div>
+
+        <h3 className="font-semibold md:text-xs lg:text-lg xl:text-lg text-indigo-800">
+          {s.name}
+        </h3>
+      </div>
+
+      <div className="col-span-2">{formatLKR(s.price)}</div>
+
+      <div className="col-span-1 text-center">{s.totalAppointments}</div>
+
+      <div className="col-span-1 text-center">{s.completed}</div>
+
+      <div className="col-span-1 text-center">{s.canceled}</div>
+
+      <div className="col-span-2 text-center">{formatLKR(earning)}</div>
+    </div>
+  );
+};
+
+export default ServiceRowDesktop;
