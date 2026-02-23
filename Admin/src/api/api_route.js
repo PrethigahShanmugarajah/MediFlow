806a9dbc61bb75ef0d06633c01df0dbf928716e7
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-23 19:21:42 +0530

    Add APPOINTMENTS_GET and APPOINTMENT_CANCEL API routes

--- Admin/src/api/api_route.js
+++ Admin/src/api/api_route.js
@@ -4,6 +4,9 @@ const BASE_URL = import.meta.env.VITE_BASEURL;
 const API_ROUTES = {
   APPOINTMENTS: {
     APPOINTMENTS_GET_REGISTERED_USERCOUNT: `${BASE_URL}/api/appointments/appointment-get-registered-usercount`,
+    APPOINTMENTS_GET: `${BASE_URL}/api/appointments/appointments-get`,
+    APPOINTMENT_CANCEL: (id) =>
+      `${BASE_URL}/api/appointments/${id}/appointment-cancel`,
   },
   DOCTORS: {
     DOCTORS_GET: `${BASE_URL}/api/doctors/doctors-get`,
