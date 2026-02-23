9e850ac92d5b246ac58a91aeb2d80d0d4dfb76f4
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-24 01:57:43 +0530

    Add service to fetch and normalize service dashboard data

---
+++ Admin/src/pages/ServiceDashboard/Service/ServiceDashboardService.jsx
@@ -0,0 +1,8 @@
+// MediFlow / Admin / src / pages / ServiceDashboard / Service / ServiceDashboardService.jsx
+import { fetchServiceAppointmentsStats } from "../../../services/fetch";
+import { buildNormalizedServicesFromResponse } from "../../../utils/serviceDashboardUtils";
+
+export async function getServiceDashboardServices() {
+  const body = await fetchServiceAppointmentsStats();
+  return buildNormalizedServicesFromResponse(body);
+}
