073b114ec1b8530b7f40dd4ab692e5c0f8429fd7
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-25 05:22:30 +0530

    Refactor AddServiceHeader to use Title component

--- Admin/src/pages/AddService/Components/AddServiceHeader.jsx
+++ Admin/src/pages/AddService/Components/AddServiceHeader.jsx
@@ -1,18 +1,15 @@
 // MediFlow / Admin / src / pages / AddService / Components / AddServiceHeader.jsx
+import Title from "../../../components/Title";
 import { CheckCircle } from "lucide-react";
 import { ClipLoader } from "react-spinners";
 
 const AddServiceHeader = ({ serviceId, submitting, onReset }) => {
   return (
     <div className="flex flex-col sm:flex-row items-start sm:items-center justify-between mb-6 sm:mb-8 gap-4">
-      <div>
-        <h1 className="text-2xl sm:text-3xl font-extrabold text-transparent bg-linear-to-r from-indigo-700 to-cyan-600 bg-clip-text">
-          {serviceId ? "Edit Service" : "Add Service"}
-        </h1>
-        <p className="text-sm text-gray-500 mt-1">
-          Create a beautiful service card with unique time slots
-        </p>
-      </div>
+      <Title
+        title={serviceId ? "Edit Service" : "Add Service"}
+        subtitle="Create a beautiful service card with unique time slots"
+      />
 
       <div className="flex w-full sm:w-auto flex-col sm:flex-row items-stretch sm:items-center gap-3">
         <button
