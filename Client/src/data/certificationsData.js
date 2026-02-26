42c4f9ba0eb1f3a0a229cebd2ab1d5a91f66b197
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-27 02:33:28 +0530

    Add certifications data with images and details

---
+++ Client/src/data/certificationsData.js
@@ -0,0 +1,84 @@
+// MediFlow / Client / src / data / certificationsData.js
+import C1 from "../assets/C1.png";
+import C2 from "../assets/C2.png";
+import C3 from "../assets/C3.png";
+import C4 from "../assets/C4.png";
+import C5 from "../assets/C5.png";
+import C6 from "../assets/C6.png";
+import C7 from "../assets/C7.png";
+import C8 from "../assets/C8.png";
+import C9 from "../assets/C9.png";
+import C10 from "../assets/C10.png";
+
+export const certificationsData = [
+  {
+    id: 1,
+    name: "Lanka Health Board",
+    image: C1,
+    alt: "Lanka Health Board Certification",
+    type: "government",
+  },
+  {
+    id: 2,
+    name: "Island Medical Authority",
+    image: C2,
+    alt: "Island Medical Authority Approval",
+    type: "government",
+  },
+  {
+    id: 3,
+    name: "Ceylon Quality Council",
+    image: C3,
+    alt: "Ceylon Quality Council Accreditation",
+    type: "healthcare",
+  },
+  {
+    id: 4,
+    name: "Lanka Clinical Commission",
+    image: C4,
+    alt: "Lanka Clinical Commission Certification",
+    type: "international",
+  },
+  {
+    id: 5,
+    name: "National Safety Federation",
+    image: C5,
+    alt: "National Safety Federation Recognition",
+    type: "healthcare",
+  },
+  {
+    id: 6,
+    name: "Lanka Medical Authority",
+    image: C6,
+    alt: "Lanka Medical Authority License",
+    type: "government",
+  },
+  {
+    id: 7,
+    name: "Asian Healthcare Council",
+    image: C7,
+    alt: "Asian Healthcare Council Certification",
+    type: "international",
+  },
+  {
+    id: 8,
+    name: "Lanka Medical Board",
+    image: C8,
+    alt: "Lanka Medical Board Approval",
+    type: "healthcare",
+  },
+  {
+    id: 9,
+    name: "Public Health Institute",
+    image: C9,
+    alt: "Public Health Institute Accreditation",
+    type: "government",
+  },
+  {
+    id: 10,
+    name: "Ceylon Medical Authority",
+    image: C10,
+    alt: "Ceylon Medical Authority Certification",
+    type: "international",
+  },
+];
