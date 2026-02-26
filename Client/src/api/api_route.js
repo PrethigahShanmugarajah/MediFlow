5c8ffa9584efc9424c011500b9680c3bf3bf4cbb
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-27 02:17:39 +0530

    Add DOCTORS_GET and SERVICES_GET API routes

--- Client/src/api/api_route.js
+++ Client/src/api/api_route.js
@@ -1,6 +1,13 @@
 // MediFlow / Client / src / api / api_route.js
 const BASE_URL = import.meta.env.VITE_BASEURL;
 
-const API_ROUTES = {};
+const API_ROUTES = {
+  DOCTORS: {
+    DOCTORS_GET: `${BASE_URL}/api/doctors/doctors-get`,
+  },
+  SERVICES: {
+    SERVICES_GET: `${BASE_URL}/api/services/services-get`,
+  },
+};
 
 export default API_ROUTES;
