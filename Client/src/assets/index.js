5f60aba8c040d15d716c8e2d8c7b157737789326
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-27 09:03:53 +0530

    Create index file to export all asset images

---
+++ Client/src/assets/index.js
@@ -0,0 +1,47 @@
+// MediFlow / Client / src / assets / index.js
+import Logo from "./Logo.png";
+import BannerImg from "./BannerImg.png";
+import NoImage from "./NoImage.png";
+import NoPersonImage from "./NoPersonImage.png";
+
+import C1 from "./C1.png";
+import C2 from "./C2.png";
+import C3 from "./C3.png";
+import C4 from "./C4.png";
+import C5 from "./C5.png";
+import C6 from "./C6.png";
+import C7 from "./C7.png";
+import C8 from "./C8.png";
+import C9 from "./C9.png";
+import C10 from "./C10.png";
+
+import TD1 from "./TD1.png";
+import TD2 from "./TD2.png";
+import TD3 from "./TD3.png";
+import TP1 from "./TP1.png";
+import TP2 from "./TP2.png";
+import TP3 from "./TP3.png";
+
+export {
+  Logo,
+  BannerImg,
+  NoImage,
+  NoPersonImage,
+  C1,
+  C2,
+  C3,
+  C4,
+  C5,
+  C6,
+  C7,
+  C8,
+  C9,
+  C10,
+  TD1,
+  TD2,
+  TD3,
+  TP1,
+  TP2,
+  TP3,
+};
+
