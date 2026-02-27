f0d231ba6f3305f9128d32edfbb33a4d4b188415
PrethigahShanmugarajah <prethigah@gmail.com>
2026-02-27 23:29:50 +0530

    Add DOCTOR_GET and APPOINTMENT_CREATE API routes

--- Client/src/api/api_route.js
+++ Client/src/api/api_route.js
@@ -4,9 +4,13 @@ const BASE_URL = import.meta.env.VITE_BASEURL;
 const API_ROUTES = {
   DOCTORS: {
     DOCTORS_GET: `${BASE_URL}/api/doctors/doctors-get`,
+    DOCTOR_GET: (id) => `${BASE_URL}/api/doctors/doctor-get/${id}`,
   },
   SERVICES: {
     SERVICES_GET: `${BASE_URL}/api/services/services-get`,
+  },
+  APPOINTMENTS: {
+    APPOINTMENT_CREATE: `${BASE_URL}/api/appointments/appointment-create`,
   },
 };
 
