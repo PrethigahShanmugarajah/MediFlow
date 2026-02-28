7a2c64bdcfea67be6da50f9d73d95e2012dcf4a4
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-01 03:44:01 +0530

    Add gender options array to helpers

--- Client/src/utils/helpers.js
+++ Client/src/utils/helpers.js
@@ -2,3 +2,10 @@
 
 /* -------- Get the currency symbol -------- */
 export const CURRENCY = import.meta.env.VITE_CURRENCY;
+
+/* -------- Options for patient gender selection -------- */
+export const genderOptions = [
+  { value: "Male", label: "Male" },
+  { value: "Female", label: "Female" },
+  { value: "Other", label: "Other" },
+];
