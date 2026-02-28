f14791bdb4590d5e2b6065699a9ab133fff66e80
PrethigahShanmugarajah <prethigah@gmail.com>
2026-03-01 03:01:17 +0530

    Add fetchServiceByID and fetchServiceBookedSlots functions

--- Client/src/services/fetch.js
+++ Client/src/services/fetch.js
@@ -123,3 +123,68 @@ export const fetchBookedSlots = async (doctorId, dateISO) => {
     throw error4;
   }
 };
+
+/* -------- Fetch Service By ID -------- */
+export const fetchServiceByID = async (id) => {
+  try {
+    if (!id) throw new Error("Service ID is required.");
+
+    const response5 = await api.get(API_ROUTES.SERVICES.SERVICE_GET(id));
+
+    const data5 = response5.data;
+    console.log("Fetch Service API Response:", data5);
+
+    if (data5?.success) {
+      // toast.success(data5?.message);
+      console.log("Fetch Service Success:", data5?.message);
+    } else {
+      toast.warn(data5?.message || "Fetch service with warning");
+      console.warn(
+        "Fetch Service Warning:",
+        data5?.message || "Fetch Service with warning",
+      );
+    }
+
+    return data5;
+  } catch (error5) {
+    toast.error(error5?.response?.data?.message || error5?.message);
+    console.error("Fetch Service Error:", error5);
+
+    throw error5;
+  }
+};
+
+/* -------- Fetch Service Booked Slots -------- */
+export const fetchServiceBookedSlots = async (serviceId, dateISO) => {
+  try {
+    if (!serviceId || !dateISO) return null;
+
+    const response6 = await api.get(
+      API_ROUTES.SERVICEAPPOINTMENTS.SERVICEAPPOINTMENTS_GET_SLOTS_BYSERVICE(
+        serviceId,
+      ),
+      { params: { date: dateISO } },
+    );
+
+    const data6 = response6.data;
+    console.log("Fetch Booked Slots API Response:", data6);
+
+    if (data6?.success) {
+      // toast.success(data6?.message);
+      console.log("Fetch Service Booked Slots Success:", data6?.message);
+    } else {
+      toast.warn(data6?.message || "Fetch service booked slots with warning");
+      console.warn(
+        "Fetch Service Booked Slots Warning:",
+        data6?.message || "Fetch Service Booked Slots with warning",
+      );
+    }
+
+    return data6;
+  } catch (error6) {
+    toast.error(error6?.response?.data?.message || error6?.message);
+    console.error("Fetch Service Booked Slots Error:", error6);
+
+    throw error6;
+  }
+};
